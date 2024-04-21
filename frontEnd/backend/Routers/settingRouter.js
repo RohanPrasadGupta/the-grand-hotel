6990ff0aee6039d91d4265e11627f9ed211e9cd4
Rohan Prasad Gupta <rohanprasadgupta4@gmail.com>
2024-04-21 17:30:50 +0700

    front end

--- frontEnd/backend/Routers/settingRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const settingController = require("../controller/settingController");
-
-const router = express.Router();
-
-router.route("/getAllSettings").get(settingController.getAllSettings);
-router.route("/createSetting").post(settingController.createSetting);
-
-module.exports = router;
